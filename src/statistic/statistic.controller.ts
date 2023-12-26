6ab15d28dbd23b53c3a26a901298164ea8fb8c9e
xdong <hy44jt@gmail.com>
2023-12-26 11:46:38 +0900

    docs: room count and booking count

--- src/statistic/statistic.controller.ts
+++ src/statistic/statistic.controller.ts
@@ -1,11 +1,30 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Inject, Query } from '@nestjs/common';
 import { StatisticService } from './statistic.service';
+import { ApiBearerAuth, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserBookingCount } from './vo/UserBookingCount.vo';
+import { MeetingRoomUsedCount } from './vo/MeetingRoomUsedCount.vo';
 
+@ApiTags('统计管理模块')
 @Controller('statistic')
 export class StatisticController {
   @Inject(StatisticService)
   private statisticService: StatisticService;
 
+  @ApiBearerAuth()
+  @ApiQuery({
+    name: 'startTime',
+    type: String,
+    description: '开始时间',
+  })
+  @ApiQuery({
+    name: 'endTime',
+    type: String,
+    description: '结束时间',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: UserBookingCount,
+  })
   @Get('userBookingCount')
   async userBookingCount(
     @Query('startTime') startTime: string,
@@ -14,6 +33,21 @@ export class StatisticController {
     return this.statisticService.userBookingCount(startTime, endTime);
   }
 
+  @ApiBearerAuth()
+  @ApiQuery({
+    name: 'startTime',
+    type: String,
+    description: '开始时间',
+  })
+  @ApiQuery({
+    name: 'endTime',
+    type: String,
+    description: '结束时间',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: MeetingRoomUsedCount,
+  })
   @Get('meetingRoomUsedCount')
   async meetingRoomUsedCount(
     @Query('startTime') startTime: string,
